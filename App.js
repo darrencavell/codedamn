7ebeed4278b1f3ab5576fcc12a77f397fccf5e17
Darren Cavell <darren_cavell@yahoo.com>
2018-12-28 12:07:34 +0700

    DEV-5 calculator part1
    calculator part1

--- App.js
+++ App.js
@@ -16,57 +16,20 @@ import {
   TextInput
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 export default class App extends Component {
-  constructor() {
+  constructor(){
     super();
     this.state = {}
-    this.buttonPressed = this.buttonPressed.bind(this);
   }
-
-  buttonPressed() {
-    // const username = this._username._lastNativeText
-    // const password = this._password._lastNativeText
-
-    console.log(this.state.username, this.state.password)
-  }
-
-  render() {
-    return (
+  render(){
+    return(
       <View style={styles.container}>
-        <Text>Username</Text>
-        <TextInput
-          defaultValue={this.state.username}
-          onChangeText={text => this.setState({username: text})}
-          />
-
-        <Text>Password</Text>
-        <TextInput
-          ref={input => this._password = input}
-          />
-
-{/* Uncontrolled Approach */}
-{/* This method works, but not efficient for React Native */}
-{/*         
-        <Text>Password</Text>
-        <TextInput
-          ref={input => this._password = input}
-          /> 
-*/}
-
-
-{/* Controlled Approach */}
-{/* Using the props that react native has provided */}
-        <Button 
-          title={'Hello'}
-          onPress={this.buttonPressed} 
-          />
+        <View style={styles.result}></View>
+        <View style={styles.calculation}></View>
+        <View style={styles.buttons}>
+          <View style={styles.numbers}></View>
+          <View style={styles.operations}></View>
+        </View>
       </View>
     );
   }
@@ -75,7 +38,25 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    padding: 20
+  },
+  result: {
+    flex: 2,
+    backgroundColor: 'red'
+  },
+  calculation:{ 
+    flex: 1,
+    backgroundColor: 'green'
+  },
+  buttons: {
+    flex: 7,
+    flexDirection: 'row'
+  },
+  numbers: {
+    flex: 4,
+    backgroundColor: 'yellow'
+  },
+  operations: {
+    flex: 1,
+    backgroundColor: 'blue'
   }
 });
