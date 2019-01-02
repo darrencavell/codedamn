fdfca481891419f8ed3d8500758426c3501f2d48
Darren Cavell <darren_cavell@yahoo.com>
2019-01-02 12:29:14 +0700

    DEV-21 custom drawer navigation
    custom drawer navigation

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import {createAppContainer, createDrawerNavigator} from 'react-navigation';
 
+import Sidebar from './app/components/Sidebar';
 import Login from './app/components/Login';
 import Calculator from './app/components/Calculator';
 
 class App extends Component {
   render(){
     return(
-      <AppStack />
+      <AppContainer />
     )
   }
 }
@@ -15,7 +16,9 @@ class App extends Component {
 const AppNavigator = createDrawerNavigator({
   login: {screen: Login},
   calculator: {screen: Calculator}
+},{
+  contentComponent: Sidebar
 })
 const AppContainer = createAppContainer(AppNavigator)
 
-export default AppContainer
+export default App
---
+++ app/components/Sidebar/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+class Sidebar extends Component {
+  render(){
+    return(
+      <View>
+        <Text>This is a nice sidebar</Text>
+      </View>
+    )
+  }
+}
+export default Sidebar
